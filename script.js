d8e1e9cdc55091391c17ec8be4242596415a3ab1
Firefox3000 <Firefox3000@users.noreply.github.com>
2020-02-14 22:41:51 +0100

    Minamizing code
    mainly removed a lines of JS
    small css and html changes

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
-let results = {
-    0: 'Steen',
-    1: 'Papier',
-    2: 'Schaar'
-}
+let y = ['steen', 'papier', 'schaar'];
 
 function steen(guess) {
     let x = Math.floor(Math.random() * 3);
-    document.querySelector('.ai').innerHTML = results[x]
-
-    if (guess == x) {
-        document.querySelector('.result').innerHTML = 'tie';
-    } else if (guess == x - 1 || guess == 2 && x == 0) {
-        document.querySelector('.result').innerHTML = 'loss';
-    } else {
-        document.querySelector('.result').innerHTML = 'win';
-    }
+    document.querySelector('.ai').innerHTML = y[x];
+    document.querySelector('.result').innerHTML = (guess == x) ? 'tie' : (guess == x - 1 || guess == 2 && x == 0) ? 'loss' : 'win';
 }
 
-document.querySelector('.steen').addEventListener('click', () => {
-    document.querySelector('.poepie .guess').innerHTML = 'steen';
-    steen(0);
-})
-
-document.querySelector('.papier').addEventListener('click', () => {
-    document.querySelector('.poepie .guess').innerHTML = 'papier';
-    steen(1);
-})
-
-document.querySelector('.schaar').addEventListener('click', () => {
-    document.querySelector('.poepie .guess').innerHTML = 'schaar';
-    steen(2);
-})
+document.querySelectorAll('.guess')[0].childNodes.forEach(item => {
+    item.addEventListener('click', (e) => {
+        document.querySelector('.poepie .guess').innerHTML = e.target.innerHTML;
+        steen(Array.prototype.slice.call(document.querySelector('.guess').children).indexOf(document.getElementsByClassName(e.target.innerHTML)[0]));
+    })
+});
 
 //for loop voor dramatisch effect
