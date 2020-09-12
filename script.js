ff8567d1f526be57b0896ca52f3da8c54da05784
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-09-12 18:57:20 +0200

    removing unnecessary js function

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-let y = ['steen', 'papier', 'schaar'];
-
-function steen(guess) {
-    let x = Math.floor(Math.random() * 3);
-    document.querySelector('.ai').innerHTML = y[x];
-    document.querySelector('.result').innerHTML = (guess == x) ? 'tie' : (guess == x - 1 || guess == 2 && x == 0) ? 'loss' : 'win';
-}
+const y = ['steen', 'papier', 'schaar'];
 
 document.querySelector('.options').addEventListener('click', (e) => {
-    let bro = Array.prototype.slice.call(document.querySelector('.options').children).indexOf(document.getElementsByClassName(e.target.innerHTML)[0])
-    document.querySelector('.guess').innerHTML = y[bro];
-    steen(bro);
+    const guess = Array.prototype.slice.call(document.querySelector('.options').children).indexOf(document.getElementsByClassName(e.target.innerHTML)[0])
+    document.querySelector('.guess').innerHTML = y[guess];
+
+    const x = Math.floor(Math.random() * 3);
+    document.querySelector('.ai').innerHTML = y[x];
+    document.querySelector('.result').innerHTML = (guess == x) ? 'tie' : (guess == x - 1 || guess == 2 && x == 0) ? 'loss' : 'win';
 });
 
-//for loop voor dramatisch effect
+//for loop for dramatic effect
